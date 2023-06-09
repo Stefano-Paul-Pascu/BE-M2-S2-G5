1ef36e37cc9c1dbc289ed3ce89db79d65b2441ca
Stefano-Paul-Pascu <124033201+Stefano-Paul-Pascu@users.noreply.github.com>
2023-06-09 16:45:23 +0200

    commit

---
+++ ProgettoSettimanale/src/main/java/stefanopascu/ProgettoSettimanale/ProgettoSettimanaleApplication.java
@@ -0,0 +1,13 @@
+package stefanopascu.ProgettoSettimanale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleApplication.class, args);
+	}
+
+}
---
+++ ProgettoSettimanale/src/test/java/stefanopascu/ProgettoSettimanale/ProgettoSettimanaleApplicationTests.java
@@ -0,0 +1,13 @@
+package stefanopascu.ProgettoSettimanale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
